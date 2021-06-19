fe976acf9ac231f1345018d1a996e90d259705a3
chanturia <george.chanturia@sportion.net>
2021-06-19 21:57:55 +0300

    frontend modifications

--- components/section2.js
+++ components/section2.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import style from '/styles/Home.module.scss'
 
-export default function section1() {
+export default function section2() {
     return (
         <div className={style.firstState}>
-            <h2>section2</h2>
-            <p>
-                Θα χαρούμε να βρεθείτε στην
-                γαμήλια γιορτή μας
-                να μοιραστείτε τη χαρά μας!
-            </p>
+            <p>παρασκευη</p>
+            <h2>13 αυγουστου 2021</h2>
+            <p>ωρα 19:30</p>
         </div>
     )
 }
--- components/section3.js
+++ components/section3.js
@@ -1,15 +1,66 @@
 import React from 'react';
 import style from '/styles/Home.module.scss'
+import styled from "styled-components";
 
-export default function section1() {
+const MainDiv = styled.div`
+  display: flex;
+`
+
+const Text1 = styled.div`
+  font-size: 2.7rem;
+  font-weight: 300;
+  margin: 27px 0px 0px 16px;
+  letter-spacing: -3px;
+`;
+const Text2 = styled.div`
+  font-size: 4.3rem;
+  font-weight: 500;
+  margin: 13px 0px 0px 16px;
+  letter-spacing: -3px;
+`;
+
+const Text3 = styled.div`
+  font-size: 3.2rem;
+  font-weight: 400;
+  margin: 13px 0 0 16px;
+  letter-spacing: -3px;`;
+
+const MapWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+const Map = styled.div`
+  border: 3px solid #274d4e;
+  border-radius: 50%;
+  height: 6rem;
+  width: 6rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.6rem;
+  font-weight: 500;
+  margin-left: 7rem;
+  cursor: pointer;
+  transition: all .4s;
+  
+  :hover{
+    background: #274d4e;
+    color: white;
+  }
+`
+export default function section3() {
     return (
-        <div className={style.firstState}>
-            <h2>section3</h2>
-            <p>
-                Θα χαρούμε να βρεθείτε στην
-                γαμήλια γιορτή μας
-                να μοιραστείτε τη χαρά μας!
-            </p>
-        </div>
+        <MainDiv className={style.firstState}>
+            <div>
+                <Text1>Κέντρο</Text1>
+                <Text2>Ypanema</Text2>
+                <Text3>Λουτράκι</Text3>
+            </div>
+            <MapWrapper>
+                <Map onClick={()=>{alert("open Mao")}}>
+                    <span>Map</span>
+                </Map>
+            </MapWrapper>
+        </MainDiv>
     )
 }

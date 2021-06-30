c8a3115a21689782511d908139066f9d32a03720
chanturia <george.chanturia@sportion.net>
2021-06-30 10:49:33 +0300

    Wake up heroku every 29 minutes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const nextTranslate = require('next-translate')
-
+const {wakeDyno} = require('heroku-keep-awake');
+const DYNO_URL = 'http://idoyes.herokuapp.com';
+const dyno_options = {
+    interval: 29,
+    logging: true,
+    stopTimes: {start: '00:00', end: '06:00'}
+}
 module.exports = nextTranslate({
     env: {
         mongodburl: "mongodb+srv://chanturia:aRmI11agOPJusptZ@cluster0.snicj.mongodb.net/yesido?retryWrites=true&w=majority",
@@ -7,6 +13,13 @@ module.exports = nextTranslate({
     future: {
         webpack5: true,
     },
+    webpack: (config, {isServer}) => {
+        if (isServer) {
+            wakeDyno(DYNO_URL, dyno_options)
+        }
+
+        return config
+    }
 })
 
 // module.exports = {
